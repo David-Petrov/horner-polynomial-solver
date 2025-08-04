3b6470dcf93185b2f63bb6d0fcb9715718237041
David Petrov <david.petrov2000@gmail.com>
2025-08-05 00:16:51 +0300

    feat: add input prompt

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use horner_solver::rational::Rational;
 use horner_solver::polynomial::Polynomial;
 
 fn main() {
+    println!("Enter space separated coefficients in ascending order:");
+
     let mut line = String::new();
     std::io::stdin().read_line(&mut line).expect("polynomial coefficients expected");
     let coefficients: Vec<i64> = line
